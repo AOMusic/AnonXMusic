715c953525ebe2598b360c8850809207644d6659
AOMusic <165138363+AOMusic@users.noreply.github.com>
2024-12-17 18:37:29 +0530

    Update misc.py
    Signed-off-by: AOMusic <165138363+AOMusic@users.noreply.github.com>

--- AnonXMusic/misc.py
+++ AnonXMusic/misc.py
@@ -1,47 +1,6 @@
-import socket
-import time
-
-import heroku3
-from pyrogram import filters
-
-import config
-from AnonXMusic.core.mongo import mongodb
-
-from .logging import LOGGER
-
-SUDOERS = filters.user()
-
-HAPP = None
-_boot_ = time.time()
-
-
+import os
 def is_heroku():
-    return "heroku" in socket.getfqdn()
-
-
-XCB = [
-    "/",
-    "@",
-    ".",
-    "com",
-    ":",
-    "git",
-    "heroku",
-    "push",
-    str(config.HEROKU_API_KEY),
-    "https",
-    str(config.HEROKU_APP_NAME),
-    "HEAD",
-    "master",
-]
-
-
-def dbb():
-    global db
-    db = {}
-    LOGGER(__name__).info(f"Local Database Initialized.")
-
-
+    return os.environ.get("DYNO", None) is not None
 async def sudo():
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
@@ -50,26 +9,20 @@ async def sudo():
     sudoers = [] if not sudoers else sudoers["sudoers"]
     if config.OWNER_ID not in sudoers:
         sudoers.append(config.OWNER_ID)
-        await sudoersdb.update_one(
-            {"sudo": "sudo"},
-            {"$set": {"sudoers": sudoers}},
-            upsert=True,
-        )
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers Loaded.")
-
-
-def heroku():
+async def heroku():
     global HAPP
     if is_heroku:
         if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
             try:
                 Heroku = heroku3.from_key(config.HEROKU_API_KEY)
-                HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"Heroku App Configured")
+                HAPP = await Heroku.app(config.HEROKU_APP_NAME)
             except BaseException:
                 LOGGER(__name__).warning(
                     f"Please make sure your Heroku API Key and Your App name are configured correctly in the heroku."
-                )
+            )
